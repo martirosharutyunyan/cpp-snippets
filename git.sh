a5b190e9c482ecf7b5f4c0e29311cc22ac94b780
martiros <harutunyan.martiros@mail.ru>
2021-11-07 17:02:45 +0400

    fixed bash script

--- git.sh
+++ git.sh
@@ -1,7 +1,11 @@
 node createREADME.js
+git fetch
+clear 
 git add .
+git status
 read -p "Enter commit name: " COMMIT
-git commit --allow-empty-message -m "${COMMIT}"
-git push
+git commit -m "${COMMIT}"
+git pull
+git push 
 rm -rf *vsix
 vsce package
